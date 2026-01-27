f2b2eef53fd1d96d8d1a914d94583e4524132d49
CuylerW <cuylerwarnock@gmail.com>
2026-01-26 21:17:26 -0500

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,21 +7,21 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1",
   "title": "Exploration 1, due February 1",
-  "body": " Exploration 1, due February 1   Exploration 1, due February 1     Choose two random vectors in which are not multiples of each other.      Choose a random third vector in and express as a linear combination of ; that is, find weights such that       Sketch the vectors in and show, geometrically, how the linear combination from (b) makes sense.        Now, choose two random vectors in which are not multiples of each other. Can every vector be expressed as a linear combination of ? Justify your answer.      Choose a random third vector that can be expressed as a linear combination of . Find appropriate weights such that        Bonus (Not graded) : Sketch the vectors in and show, geometrically, how the linear combination from (b) makes sense.      "
+  "body": " Exploration 1, due February 1   Exploration 1, due February 1  Make sure to familariaze yourself with the Exploration Instructions found on the home page.     Choose two random vectors in which are not multiples of each other.      Choose a random third vector in and express as a linear combination of ; that is, find weights such that       Sketch the vectors in and show, geometrically, how the linear combination from (b) makes sense.        Now, choose two random vectors in which are not multiples of each other. Can every vector be expressed as a linear combination of ? Justify your answer.      Choose a random third vector that can be expressed as a linear combination of . Find appropriate weights such that        Bonus (Not graded) : Sketch the vectors in and show, geometrically, how the linear combination from (b) makes sense.      "
 },
 {
-  "id": "exp-1-2",
+  "id": "exp-1-3",
   "level": "2",
-  "url": "explorations-3.html#exp-1-2",
+  "url": "explorations-3.html#exp-1-3",
   "type": "Problem",
   "number": "1.1",
   "title": "",
   "body": "   Choose two random vectors in which are not multiples of each other.      Choose a random third vector in and express as a linear combination of ; that is, find weights such that       Sketch the vectors in and show, geometrically, how the linear combination from (b) makes sense.    "
 },
 {
-  "id": "exp-1-3",
+  "id": "exp-1-4",
   "level": "2",
-  "url": "explorations-3.html#exp-1-3",
+  "url": "explorations-3.html#exp-1-4",
   "type": "Problem",
   "number": "1.2",
   "title": "",
